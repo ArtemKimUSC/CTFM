8784df2d2dddaeafbb7fd50a9cdba4e7403aa6ca
ArtemKimUSC <artemkim@usc.edu>
2024-10-11 10:15:23 -0700

    uploaded cancer sldsc annots

---
+++ install/download_cancer_reference.sh
@@ -0,0 +1,17 @@
+
+SCRIPT_PATH=$(dirname $0)
+DATA_PATH=$SCRIPT_PATH/../data
+
+
+
+cd $DATA_PATH
+
+echo "Downloading S-LDSC cancer annotations for CT-FM"
+
+wget https://zenodo.org/records/13914819/files/SLDSC_cancer.tar.gz?download=1 -O SLDSC_cancer.tar.gz
+
+tar -xvzf SLDSC_cancer.tar.gz
+
+rm SLDSC_cancer.tar.gz
+
+echo "Done"
